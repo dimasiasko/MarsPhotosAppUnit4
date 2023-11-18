1c37548c095c62e68d31062feb70a5a566d20178
Dmytro Siasko <dima.siasko@gmail.com>
2023-11-18 16:50:28 +0200

    Added binding adapters, main activity implemented

--- app/src/main/java/com/example/marsphotosappunit4/MainActivity.kt
+++ app/src/main/java/com/example/marsphotosappunit4/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.marsphotosappunit4
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
